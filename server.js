b795983e1947c77f8f6d64b29a6ebea89494d8b5
alerodmor@hotmail.com <alerodmor@hotmail.com>
2022-10-17 20:08:29 +0300

    xdd

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-14-crud-example-master'));
+app.use(express.static('./dist/angular-14-crud-example'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-14-crud-example-master/'}),
+    res.sendFile('index.html', {root: 'dist/angular-14-crud-example/'}),
 );
 
 // Start the app by listening on the default Heroku port
